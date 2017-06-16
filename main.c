970be8624fe4785b7d25025f97781f50d3186919
fabianji <fabian.jimenez@sansano.usm.cl>
2017-06-16 15:11:22 -0400

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+
+
+int main(){
+	FILE *fp;
+	int num_obj, peso_max, i;
+
+	fp = fopen("input-edd-t3.txt","r");
+
+	fscanf(fp,"%d",&num_obj);
+
+	int *precios=(int*)malloc(sizeof(int)*num_obj);
+	int *pesos=(int*)malloc(sizeof(int)*num_obj);
+	
+	if (precios==NULL || pesos==NULL)	printf("error en manejo de memoria precios o pesos");
+
+	for(i=0; i<num_obj; i++)	fscanf(fp,"%d",&precios[i]);
+	
+	for(i=0; i<num_obj; i++)	fscanf(fp,"%d",&pesos[i]);
+		
+	fscanf(fp,"%d",&peso_max);
+//-------------------
+
+
+
+
+
+
+
+
+
+//-------------------
+	fclose(fp);
+	free(precios);
+	free(pesos);
+	return 0;
+
+
+}
